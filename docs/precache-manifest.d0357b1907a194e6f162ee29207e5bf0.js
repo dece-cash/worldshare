3c20f5aa5b25f2746b637a95b6fbc17ed25ab0a2
dececash <dececash@protonmail.com>
2020-10-03 22:11:41 +0800

    add docs

---
+++ docs/precache-manifest.d0357b1907a194e6f162ee29207e5bf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fff966d03b3020d9a67a54ee584d752",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0fc27fb4d63253080382",
+    "url": "./static/css/2.f4b9661e.chunk.css"
+  },
+  {
+    "revision": "56d5cb76c2d5fedea019",
+    "url": "./static/css/main.008e8c86.chunk.css"
+  },
+  {
+    "revision": "0fc27fb4d63253080382",
+    "url": "./static/js/2.78c7b58a.chunk.js"
+  },
+  {
+    "revision": "5c172b900f87edf84596ace20fc6bef7",
+    "url": "./static/js/2.78c7b58a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56d5cb76c2d5fedea019",
+    "url": "./static/js/main.80ca5bcd.chunk.js"
+  },
+  {
+    "revision": "3d68944d0afd8b596819",
+    "url": "./static/js/runtime-main.2b7bf7f5.js"
+  }
+]);
